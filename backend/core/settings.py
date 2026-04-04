353fc1bbf7376469dee2ced63701a48c32c461b5
joe john <joejohn@Lappy.local>
2026-04-04 14:20:55 +0530

    changes fixed bugs

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -107,7 +107,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+        'core.authenticators.CsrfExemptSessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ],
 }
