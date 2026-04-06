4ad340c0c27a056e2f6f4d1a728a77aaf387a5ab
joe john <joejohn@Lappy.local>
2026-04-06 18:02:20 +0530

    added history

--- backend/apps/votes/serializers.py
+++ backend/apps/votes/serializers.py
@@ -2,8 +2,12 @@ from rest_framework import serializers
 from .models import Vote
 
 
+from apps.cases.serializers import CaseSerializer
+
 class VoteSerializer(serializers.ModelSerializer):
+    case_details = CaseSerializer(source='case', read_only=True)
+    
     class Meta:
         model = Vote
-        fields = ['id', 'case', 'voter', 'decision', 'created_at']
+        fields = ['id', 'case', 'case_details', 'voter', 'decision', 'created_at']
         read_only_fields = ['voter', 'created_at']
