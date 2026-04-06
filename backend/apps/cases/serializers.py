015025d2cdd7c857872636c03c5b6e3642bd1bef
joe john <joejohn@Lappy.local>
2026-04-06 12:08:43 +0530

    added new pages

--- backend/apps/cases/serializers.py
+++ backend/apps/cases/serializers.py
@@ -6,6 +6,19 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'slug']
+        extra_kwargs = {'slug': {'required': False}}
+
+    def validate_slug(self, value):
+        if not value:
+            from django.utils.text import slugify
+            return slugify(self.initial_data.get('name', ''))
+        return value
+
+    def create(self, validated_data):
+        if not validated_data.get('slug'):
+            from django.utils.text import slugify
+            validated_data['slug'] = slugify(validated_data.get('name', ''))
+        return super().create(validated_data)
 
 
 class CaseSerializer(serializers.ModelSerializer):
